09e35103d2cdb30393c14c286c3a358663dd875f
Apichart Chongcharoen <chartchuo@gmail.com>
2020-06-05 20:17:10 +0700

    change webview library

--- lib/webview_normal.dart
+++ lib/webview_normal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+// import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:easy_web_view/easy_web_view.dart';
 import 'model.dart';
 
 class WebviewNorm extends StatefulWidget {
@@ -11,11 +12,22 @@ class _WebState extends State<WebviewNorm> {
   @override
   Widget build(BuildContext context) {
     final ScanedUrl args = ModalRoute.of(context).settings.arguments;
-    return WebviewScaffold(
-      appBar: AppBar(
-        title: const Text('WebView'),
+    // return WebviewScaffold(
+    //   appBar: AppBar(
+    //     title: const Text('WebView'),
+    //   ),
+    //   url: args.url,
+    // );
+    return Scaffold(
+      appBar: AppBar(title: const Text('Webview')),
+      body: EasyWebView(
+        src: args.url, onLoaded: () {},
+        // isHtml: false, // Use Html syntax
+        // isMarkdown: false, // Use markdown syntax
+        // convertToWidets: false, // Try to convert to flutter widgets
+        // width: 100,
+        // height: 100,
       ),
-      url: args.url,
     );
   }
 
